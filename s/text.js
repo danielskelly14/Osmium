4c5da55a381a7e84cd22ac00ce6cf3dd448a5d8b
Marko <marko.vityk@gmail.com>
2026-04-17 21:14:03 -0500

    redo file structure and a couple extra updates

--- s/text.js
+++
@@ -1,4 +0,0 @@
-// contains all messages on front page
-let quotes = [
-    "Help us make this website by joining our discord server!",
-]
---
+++ text.js
@@ -0,0 +1,4 @@
+// contains all messages on front page
+let quotes = [
+    "Help us make this website by joining our discord server!",
+]
